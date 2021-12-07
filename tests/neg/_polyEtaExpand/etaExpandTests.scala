e5383183be2f40fe13a4e0ad7e40a95aa9ec5639
Quentin Bernet <quentin.bernet@bluewin.ch>
2021-12-07 17:52:48 +0100

    Fix and Cleanup tests

--- tests/neg/_polyEtaExpand/etaExpandTests.scala
+++ tests/neg/_polyEtaExpand/etaExpandTests.scala
@@ -2,13 +2,13 @@
 
 class EtaExpansionTests{
         
-    def id[T](x: T) = x // method type [T](T): T
-    val valId5: Int => Boolean = id // error: Found: (x: Int), Required: Boolean // unintuitive error !
+    def id[T](x: T) = x
+    val valId5: Int => Boolean = id // error
 
-    def weird1[T](x: T): Any = ??? // method type [T](T): Any
-    val valWeird1: [T] => T => T = weird1 // error, but expected
+    def weird1[T](x: T): Any = ???
+    val valWeird1: [T] => T => T = weird1 // error
     
-    def weird4[T](x: Nothing): T = ??? // method type [T](Nothing): T
+    def weird4[T](x: Nothing): T = ???
     val valWeird4: [T] => T => T = weird4 //error
 }
 
