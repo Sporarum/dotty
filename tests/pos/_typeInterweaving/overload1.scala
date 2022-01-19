ef552bb4db65f684da89878ecc56ec6fa616f241
Quentin Bernet <quentin.bernet@bluewin.ch>
2022-01-19 18:08:48 +0100

    Remove temporary tests

--- tests/pos/_typeInterweaving/overload1.scala
+++
@@ -1,6 +0,0 @@
-
-class A{
-    def f1[T](x: Any) = ???
-    def f1[T](x: Int) = ???
-    f1(1)
-}
--- tests/pos/_typeInterweaving/overload2.scala
+++
@@ -1,6 +0,0 @@
-
-class A{
-    def f1[T][U](x: Any) = ???
-    def f1[T][U](x: Int) = ???
-    f1(1)
-}
