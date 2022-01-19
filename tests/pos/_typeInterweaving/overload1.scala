2e28a4d7a8cb175df9ae7df3b105238d80366922
Quentin Bernet <quentin.bernet@epfl.ch>
2022-01-19 14:51:51 +0100

    WIP

---
+++ tests/pos/_typeInterweaving/overload1.scala
@@ -0,0 +1,6 @@
+
+class A{
+    def f1[T](x: Any) = ???
+    def f1[T](x: Int) = ???
+    f1(1)
+}
---
+++ tests/pos/_typeInterweaving/overload2.scala
@@ -0,0 +1,6 @@
+
+class A{
+    def f1[T][U](x: Any) = ???
+    def f1[T][U](x: Int) = ???
+    f1(1)
+}
