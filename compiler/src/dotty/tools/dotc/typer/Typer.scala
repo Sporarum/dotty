16e52629ae8823f95c8ef60e5f6028b542a55529
Quentin Bernet <quentin.bernet@epfl.ch>
2023-02-01 16:38:29 +0100

    Cleanup

--- compiler/src/dotty/tools/dotc/typer/Typer.scala
+++ compiler/src/dotty/tools/dotc/typer/Typer.scala
@@ -4095,32 +4095,21 @@ class Typer(@constructorOnly nestingLevel: Int = 0) extends Namer
               adaptOverloaded(ref)
           }
         case poly: PolyType if !(ctx.mode is Mode.Type) =>
-          if isApplyProxy(tree) then 
-            // println("isApplyProxy")
+          if isApplyProxy(tree) then
             newExpr
-          else if pt.isInstanceOf[PolyProto] then 
-            // println("isPolyProto")
+          else if pt.isInstanceOf[PolyProto] then
             tree
-          else if !pt.exists then // no expected type
-            // println("no expected type")
-            ???
           else
-            // println("else")
             pt match
               case RefinedType(_, _, npt: PolyType) => // Should be something that matches specifically PolyFunction instead
                 print(s"""
-                |Trying to adapt1 the following:
-                |${poly.show}
-                |$poly
-                |with expected type:
-                |${pt.show}
-                |$pt
-                |""".stripMargin)
-  
-
-                // val tparamNames: List[TypeName] = npt.paramNames
-                // val typeOfApply = PolyType(tparamNames)(tl => npt.paramInfos.subst(), computeResult)
-                // val typeOfApply = npt
+                  |Trying to adapt1 the following:
+                  |${poly.show}
+                  |$poly
+                  |with expected type:
+                  |${pt.show}
+                  |$pt
+                  |""".stripMargin)
 
                 def applyMethod(owner: Symbol): DefDef =
                   val applySym = Symbols.newSymbol(owner, nme.apply, Method | Synthetic, npt, coord = tree.span).entered
@@ -4128,105 +4117,22 @@ class Typer(@constructorOnly nestingLevel: Int = 0) extends Namer
                   tpd.DefDef(applySym, {
                     case tparams :: vparams :: Nil =>
                       typed(untpd.TypeApply(untpd.TypedSplice(tree), tparams.map(untpd.TypedSplice(_))),
-                        // TODO: substitute vparams to handle dependent methods (e.g., `[T] => (x: T) => x.A`)
+                        // TODO: substitute vparams to handle dependent methods (e.g., with type `[T] => (x: T) => x.A`)
                         npt.instantiate(tparams.tpes).toFunctionType(isJava = false)
                       )
                     case _ => ???
                   })
                 val t = tpd.AnonClass(ctx.owner, List(defn.PolyFunctionType), tree.span)(clsSymbol => List(applyMethod(clsSymbol)))
-                val s = tpd.Typed(t, TypeTree(pt))
-                s
-
-                // //println()
-                // //println(npt.paramRefs.map(_.show))
-                // //println(npt.paramRefs)
-
-                // // use DefDef that takes two args, the symbol of the def, and a lambda that returns the body of the def from it's parameters
+                val res = tpd.Typed(t, TypeTree(pt))
                 
-                // val functionTypeParamBounds: List[TypeBounds] = npt.paramInfos // we use this even though we should use a etaTypeParamBounds with the names replaced by the fresh ones
-                // val functionTypeParamNames: List[TypeName] = npt.paramNames
-                // val subFunction = npt.resType.asInstanceOf[MethodType]
-
-                // val functionTermParams: List[Type] = subFunction.paramInfos
-                // val functionTermParamNames: List[TermName] = subFunction.paramNames
-
-
-                // val etaTypeParamNames = functionTypeParamNames.map( UniqueName.fresh(_) )
-
-                // /*val etaTypeParamsOld = (etaTypeParamNames zip functionTypeParamBounds).map{
-                //   case (name, bounds) => untpd.TypeDef(name, untpd.TypeTree(bounds)).withAddedFlags(Param)
-                // }*/
-
-                // //println()
-                // //println(etaTypeParamsOld.map(_.show))
-                // //println(etaTypeParamsOld)
-                // //println(etaTypeParamsOld.map(_.name))
-                // //println(etaTypeParamsOld.map(_.symbol)) // .symbol should be defined
-
-
-                // // val etaTypeSymbols = typeOfApply.paramSymss.head//Symbols.newTypeParams(owner = ctx.owner, etaTypeParamNames, Param, _ => functionTypeParamBounds)
-
-                // // val etaTypeParams2 = etaTypeSymbols.map( sym => tpd.TypeDef(sym) )
-
-                // //println()
-                // //println(etaTypeSymbols.map(_.show))
-                // //println(etaTypeSymbols)
-                // //println(etaTypeSymbols.map(_.name))
-
-                // // val typeRefArgs = etaTypeSymbols.map{ TypeRef(NoPrefix, _) }
-                // //println()
-                // //println(typeRefArgs.map(_.show))
-                // //println(typeRefArgs)
-                // def computeResult(p: PolyType): Type = poly.appliedTo(p.paramRefs)
-                // val typeOfApply = PolyType(etaTypeParamNames)(_ => functionTypeParamBounds, computeResult) // Should use the lambdas, they give us the etaTypes, we shouldn't try to create them
-                
-                // val subMethod = typeOfApply.resType.asInstanceOf[MethodType]//poly.appliedTo(typeRefArgs).asInstanceOf[MethodType]
-                // //println()
-                // //println(poly.show)
-                // //println(poly)
-                // //println()
-                // //println(poly.resultType.show)
-                // //println(poly.resultType)
-                // //println()
-                // //println(subMethod.show)
-                // //println(subMethod)
-
-                // val etaTermParamNames = subMethod.paramNames.map( UniqueName.fresh(_) )
-
-                // val methodTermParams = subMethod.paramInfos
-                // //println(methodTermParams.map(_.show))
-                // val etaTermSymbols = (etaTermParamNames zip methodTermParams).map( (name, tpe) => Symbols.newSymbol(ctx.owner, name, Param, tpe, coord = tree.span) )
-
-                // val etaTermParams = etaTermSymbols.map(sym => tpd.ValDef(sym))
-
-                // val etaTermNamedTypes = etaTermSymbols.map( sym => NamedType(NoPrefix, sym) )
-                // val termArgs = etaTermNamedTypes.map( p => tpd.Ident(p) )
-
-
-
-                // val res = ???
-
-                // // val applySymbol: Symbol => TermSymbol = owner => Symbols.newSymbol(owner, nme.apply, Method, typeOfApply, coord = tree.span) // flag Synthetic ?
-                // // val paramss: List[List[Symbol]] = List( etaTypeSymbols, etaTermSymbols )
-                // // val body: tpd.Tree = tree.appliedToTypes(typeRefArgs).appliedToArgs(termArgs)
-                // // val applyMethod: Symbol => tpd.DefDef = owner => tpd.DefDef(applySymbol(owner), paramss, typeOfApply.finalResultType, body)
-                // // val res = tpd.AnonClass(ctx.owner, List(defn.PolyFunctionType), tree.span)(clsSymbol => List(applyMethod(clsSymbol)))
-
-                // //val tArgs = typeRefArgs.map(ref => tpd.TypeTree(ref)) 
-                
-                // // tree[tArgs](termArgs)
-
-                // // [etaTypeParamsOld] => (etaTermParams) => tree[tArgs](termArgs)
-                // //val res = tpd.Lambda(subMethod, termArgs => tree.appliedToTypes(typeRefArgs).appliedToArgs(termArgs))//tpd.PolyFunction(etaTypeParams2, tpd.Function(etaTermParams, body))
-                // print(s"""
-                //          |Result:
-                //          |${res.show}
-                //          |$res
-                //          |""".stripMargin)
-                // res//typed(res, pt)
-                  
+                println(s"""
+                  |yielded:
+                  |${res.show}
+                  |$res
+                  |""".stripMargin)
+                res
+ 
               case _ =>
-                //TODO
                 var typeArgs = tree match
                   case Select(qual, nme.CONSTRUCTOR) => qual.tpe.widenDealias.argTypesLo.map(TypeTree(_))
                   case _ => Nil
